c30b9e8f0812c6a8f5aefea59c3b53899da3c3a1
damindda <damindda@gmail.com>
2022-06-27 16:56:15 +0100

    test: router and http module

--- src/app/modules/user-details/user-details/user-details.component.spec.ts
+++ src/app/modules/user-details/user-details/user-details.component.spec.ts
@@ -1,16 +1,26 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserDetailsComponent } from './user-details.component';
+import { GitHubUsersService } from 'src/app/services/git-hub-users.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute, Router } from '@angular/router'
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
+  let router: Router;
+  let activateRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [GitHubUsersService],
       declarations: [ UserDetailsComponent ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      router = TestBed.inject(Router);
+      activateRoute = TestBed.inject(ActivatedRoute);
+    })
   });
 
   beforeEach(() => {
