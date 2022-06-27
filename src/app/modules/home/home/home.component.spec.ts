c8f6c269eed2f088c8b2e9a8bdca8ecc28e833f0
damindda <damindda@gmail.com>
2022-06-27 16:55:10 +0100

    test: ngrx store and effects

--- src/app/modules/home/home/home.component.spec.ts
+++ src/app/modules/home/home/home.component.spec.ts
@@ -1,20 +1,44 @@
+import { Observable } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Actions, EffectsModule } from '@ngrx/effects';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let store: MockStore<{ allusers: [] }>;
+  const initialState = {
+    error: null,
+    allusers: [],
+    user: null
+  }
+  let actions: Observable<Actions>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+      ],
+      declarations: [HomeComponent],
+      providers: [
+        provideMockStore({
+          initialState: initialState
+        }),
+        provideMockActions(() => actions)
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
+    store = TestBed.inject(MockStore)
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
