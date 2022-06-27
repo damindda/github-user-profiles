4d7fd8b7a6e39e4c18fd58aa32be91ab02b55f12
damindda <damindda@gmail.com>
2022-06-27 17:05:22 +0100

    feat: user details service bind with async

--- src/app/modules/user-details/user-details/user-details.component.ts
+++ src/app/modules/user-details/user-details/user-details.component.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription, shareReplay } from 'rxjs';
 import { UserDetails } from 'src/app/models/user-details';
 import { GitHubUsersService } from 'src/app/services/git-hub-users.service';
-
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserDetailsComponent implements OnInit {
-  user!: UserDetails;
+export class UserDetailsComponent implements OnInit, OnDestroy {
+  user$!: Observable<UserDetails>;
+  subscription!: Subscription;
+  userName!: string;
+
   constructor(
     private gitHubUsersService: GitHubUsersService,
     private activateRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe((data) => {
-      const userName = data['id'];
-      console.log('params---->', data['id']);
-      this.getUserData(userName);
+    this.subscription = this.activateRoute.params.subscribe((data) => {
+      this.userName = data['id'];
+      this.getUserData(this.userName);
     });
   }
 
   getUserData(userName: string) {
-    this.gitHubUsersService
-      .getUser(userName)
-      .subscribe((data: UserDetails) => this.user = data);
+      this.user$ = this.gitHubUsersService.getUser(userName).pipe(shareReplay(1));
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
   }
 }
