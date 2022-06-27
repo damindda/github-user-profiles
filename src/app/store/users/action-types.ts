03afe67fafd0e948300d7e1f2858d750c22b4fc6
damindda <damindda@gmail.com>
2022-06-27 17:20:42 +0100

    feat: action types enum

---
+++ src/app/store/users/action-types.ts
@@ -0,0 +1,5 @@
+export enum UserActionTypes {
+  GET_ALL_USERS = '[USERS PAGE] Get All Users',
+  GET_ALL_USERS_SUCCESS = '[USERS PAGE] Get All Users Success',
+  GET_ALL_USERS_ERROR = '[USERS PAGE] Get All Users Error'
+}
