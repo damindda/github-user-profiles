b5ae34820349d8a7ed5b2eb137c42a58eaaa17b6
damindda <damindda@gmail.com>
2022-06-27 17:20:22 +0100

    feat: all users success data action

--- src/app/store/users/actions.ts
+++ src/app/store/users/actions.ts
@@ -1,9 +1,20 @@
 import { createAction, props } from '@ngrx/store';
 import { UserActionTypes } from './action-types';
-
+import { User } from 'src/app/models';
 
 export const getAllUsersAction = createAction(
   UserActionTypes.GET_ALL_USERS,
   props<{ pageNumber: number, perPage: number }>()
 )
+
+export const getAllUsersSuccessAction = createAction(
+  UserActionTypes.GET_ALL_USERS_SUCCESS,
+  props<{ data: User[]}>()
+)
+
+export const getAllUsersErrorAction = createAction(
+  UserActionTypes.GET_ALL_USERS_ERROR,
+  props<{ error: string}>()
+)
+
 
